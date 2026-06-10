388adcf2973951bb75a63dd91cdbc3fab885055f
a1729200350 <a1729200350@gmail.com>
2026-06-10 16:31:27 +0800

    新增02-引用章节，更新README

---
+++ C++核心编程/02 引用/02 C++引用注意事项.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include<string>
+using namespace std;
+int main2()
+{
+	int a = 10;
+
+	//1、引用必须初始化
+	// 	int& b;   错误 必须要初始化
+	int& b = a;
+	//2、引用在初始化后，不可以改变
+	int c = 50;
+	b = c;  //赋值操作，而不是更改引用
+	cout << "a: " << a << endl;
+	cout << "b: " << b << endl;
+	cout << "c: " << c << endl;
+	system("pause");
+	return 0;
+}
