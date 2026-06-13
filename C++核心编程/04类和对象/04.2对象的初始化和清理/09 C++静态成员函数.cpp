ed89f290d5359b9dacac45c1e361b64b59f0974e
a1729200350 <a1729200350@gmail.com>
2026-06-13 16:22:06 +0800

    新增04.2-静态成员变量/函数，新增04.3-对象模型和this指针，更新README

---
+++ C++核心编程/04类和对象/04.2对象的初始化和清理/09 C++静态成员函数.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<string>
+using namespace std;
+//静态成员函数
+//1、所有的对象共享同一个函数
+//2、静态成员函数只能访问静态成员变量
+class Person
+{
+public:
+	//静态成员函数
+	static void func()
+	{	
+		m_A = 100;		//静态成员函数可以访问 静态成员变量
+		//m_B = 1000;	//静态成员函数不可以访问 非静态成员变量 ，无法区分到底是是那个对象的m_B属性
+		cout << "satic void func调用" << endl;
+	}
+	static int m_A;
+	int m_B;//非静态成员变量
+	//静态成员函数也是有访问权限的
+private:
+	static void func2()
+	{
+		cout << "static void func2调用" << endl;
+	}
+};
+ int Person::m_A=0;
+//有两种访问方式
+void test01()
+{
+	//1、通过对象访问
+	Person p;
+	p.func();
+	//2、通过类名访问
+	Person::func();
+	//Person::func2();		private 类外访问不到私有静态成员函数
+}
+int main()
+{
+	test01();
+	system("pause");
+	return 0;
+}
